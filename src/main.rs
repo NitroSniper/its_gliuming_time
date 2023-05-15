3d13ddf707a56afc0d239559c1c70c563b96b5af
NitroSniper <mahiemiah35@gmail.com>
2023-05-15 09:25:52 +0100

    removed a file. and made a window in glium!

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,22 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let wb = glutin::window::WindowBuilder::new();
     let display = glium::Display::new(wb, cb, &events_loop).expect("Display could not be created.");
+
+
+    events_loop.run(move |ev, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() + 
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+
+            }
+            _ => (),
+        }
+    });
 }
